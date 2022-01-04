ffa7e1417341f9efdcda9f81cc4796c17afeb086
brunnacroches <brunnacroches@gmail.com>
2022-01-03 23:26:19 -0300

    commit on master

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { preloadImages, preloadFonts } from './utils'
 
-import * as LocomotiveScroll from 'https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.min.css';
+import * as LocomotiveScroll from 'https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.3/dist/locomotive-scroll.js';
 
 import * as Splitting from 'https://cdn.jsdelivr.net/npm/splitting@1.0.6/dist/splitting-lite.min.js';
 
